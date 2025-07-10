MotorolaMobilityLLC/vendor-qcom-opensource-camera-kernel

msm: camera: icp: Add validation for memory mapping
Before sending mem mapping info to FW, this change adds
validation between buffer size and offset provided by
userspace to aviod accessing unmapped region.

CRs-Fixed: 4092273
Change-Id: Iefe052a402add055ee9f3ca6061526436ed7a75c
Signed-off-by: Haochen Yang <haocyang@qti.qualcomm.com>

drivers/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
@@ -6641,6 +6641,17 @@ static int cam_icp_process_stream_settings(
 			return -EINVAL;
 		}
 
+		if (cmd_mem_regions->map_info_array_flex[i].offset >= len) {
+			CAM_ERR(CAM_ICP,
+				"%s: Mem region offset exceeds length of the buffer, mem handle: 0x%x, iova: %pK, len: %u, offset: 0x%x",
+				ctx_data->ctx_id_string,
+				cmd_mem_regions->map_info_array_flex[i].mem_handle,
+				(uint32_t)iova, (uint32_t)len,
+				cmd_mem_regions->map_info_array_flex[i].offset);
+			CAM_MEM_FREE(map_cmd);
+			return -EINVAL;
+		}
+
 		/* FW/CDM buffers are expected to be mapped in 32-bit address range */
 		map_cmd->mem_map_region_sets_flex[i].start_addr = (uint32_t)iova +
 			(cmd_mem_regions->map_info_array_flex[i].offset);

lolipuru/kernel_xiaomi_sm8650-modules

(cherry picked from commit 7c002bbc24ecdc8d5c70b29e18337316687c8d96)

drivers/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
@@ -5700,6 +5700,17 @@ static int cam_icp_process_stream_settings(
 			return -EINVAL;
 		}
 
+		if (cmd_mem_regions->map_info_array[i].offset >= len) {
+			CAM_ERR(CAM_ICP,
+				"%s: Mem region offset exceeds length of the buffer, mem handle: 0x%x, iova: %pK, len: %u, offset: 0x%x",
+				ctx_data->ctx_id_string,
+				cmd_mem_regions->map_info_array[i].mem_handle,
+				(uint32_t)iova, (uint32_t)len,
+				cmd_mem_regions->map_info_array[i].offset);
+			kfree(map_cmd);
+			return -EINVAL;
+		}
+
 		/* FW/CDM buffers are expected to be mapped in 32-bit address range */
 		map_cmd->mem_map_region_sets[i].start_addr = (uint32_t)iova +
 			(cmd_mem_regions->map_info_array[i].offset);